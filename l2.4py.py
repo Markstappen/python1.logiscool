8fa16b4f2dc82f3fe1b5f66ee06ea81d240eab75
Markstappen <makay.mark09@gmail.com>
2026-03-07 18:09:59 +0100

    Add files via upload

---
+++ l2.4py.py
@@ -0,0 +1,33 @@
+import random
+
+def task1():
+    nums = []
+    while True:
+        num=random.randint(1,100)
+        nums.append
+        if len(nums)>=10:
+            break
+    return nums
+
+nums = task1()
+def task2(nums):
+    evenl=[]
+    for i in nums:
+        if i % 2 == 0:
+            evenl.append(i)
+    return evenl       
+print(task2(nums))
+
+
+task2(nums)
+
+
+
+def task3(nums):
+    
+    for i in nums:
+        if i % 2 == 0:
+            yield i       
+print(task3(nums))
+
+
