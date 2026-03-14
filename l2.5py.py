797edfbbc07223eec4899d7e5a4c8205cf3b9885
Markstappen <makay.mark09@gmail.com>
2026-03-14 12:30:36 +0100

    Add files via upload

---
+++ l2.5py.py
@@ -0,0 +1,29 @@
+
+
+def create_matrix():
+    matrlx = []
+    for i in range(4):
+        temp = []
+        for j in range(4):
+            temp.append(j)
+        matrix.append(temp)
+    print(matrix)
+    
+    # string -> number
+    # number -> string
+    # number -> boolean
+
+
+    def modifyMatrix():
+        matrix = [
+            [1, True, 3, 'd', 5],
+            [45.4, 2, 9, 'b', 6],
+            [99, 22, 'a', 4, 'g'],
+            [5, 12, 3, 'k', 'e'],
+            [0, 21, 35, False, 56] 
+        ]
+    print(matrix)
+    input(Tell me the index of the information you want to change and to what)
+    
+    
+          
