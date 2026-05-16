6c4863c25ea435312d34e22d4096f6f0c3905bed
Markstappen <makay.mark09@gmail.com>
2026-05-16 18:18:19 +0200

    Add files via upload

---
+++ L12.py
@@ -0,0 +1,36 @@
+def openFile():
+    file = open("test.txt","r")
+    data = file.read()
+    print(data)
+    file.close()
+openFile()
+
+def writeReadFile():
+    file = open("test.txt","a+")
+    file.write(" helle le frere de stef")
+    file.seek(0)
+    data = file.read
+    print(data)
+    file.close()
+openFile()
+writeReadFile()
+def readLines():
+    file = open("test.txt","r")
+    data1 = file.read()
+    file.seek(0)
+    data2 = file.readlines()
+    file.seek(0)
+    data3 = file.readline()
+    print(data1)
+    print(data2)
+    print(data3)
+readLines()
+
+def Rline():
+    file = open("test.txt","r")
+    data = file.readlines()
+    for line in data:
+        print(line)
+Rline()
+
+
